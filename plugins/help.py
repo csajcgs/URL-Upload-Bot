12e4e34eec03292e0d23ddba6a048f45ec13e92e
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-07-31 19:18:10 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -5,54 +5,18 @@
 import logging
 import os
 from translation import Translation as tr
-from config import Config as C
+from config import Config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
-UPDATES_CHANNEL = C.UPDATES_CHANNEL
+
 logging.basicConfig(level=logging.INFO)
 
 
 @Client.on_message(filters.incoming & filters.command(['help']))
 def _help(client, message):
-    update_channel = UPDATES_CHANNEL
-    if update_channel:
-        try:
-            user = client.get_chat_member(update_channel, message.chat.id)
-            if user.status == "kicked":
-               client.send_message(
-                   chat_id=message.chat.id,
-                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/UniversalBotsSupport).",
-                   parse_mode="markdown",
-                   disable_web_page_preview=True
-               )
-               return
-        except UserNotParticipant:
-            client.send_message(
-                chat_id=message.chat.id,
-                text="**Please Join My Updates Channel to use this Bot!**",
-                disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
-                        ]
-                    ]
-                ),
-                parse_mode="markdown"
-            )
-            return
-        except Exception:
-            client.send_message(message.chat.id,
-                text=tr.HELP_MSG,
-	        disable_web_page_preview=True,
-        parse_mode="markdown",
-        reply_to_message_id=message.message_id
-        )
-            return
-            client.send_message(message.chat.id,
-                text=tr.HELP_MSG,
-                disable_web_page_preview=True,
+      client.send_message(message.chat.id,
+        text=tr.HELP_MSG,
+        disable_web_page_preview=True,
         parse_mode="markdown",
         reply_to_message_id=message.message_id
         )
