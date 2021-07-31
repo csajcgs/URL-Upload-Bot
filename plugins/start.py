e214d6eb1a2048317de1ab2cc1376e71a703fa7d
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-07-31 09:45:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from translation import Translation
+from translation import Translation as tr
 from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
 
 if bool(os.environ.get("WEBHOOK", False)):
@@ -55,9 +55,8 @@ async def start(client, message):
             )
             return
         except Exception:
-        await client.send_message(
-            chat_id=update.chat.id,
-            text=Translation.START_TEXT.format(update.from_user.first_name),
+        await client.send_message(chat_id=message.chat.id,
+            text=tr.START_TEXT.format(update.from_user.first_name),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
