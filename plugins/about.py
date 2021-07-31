85d7796e22118724bcf0f3ea95f9d772d631c99c
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-07-31 19:23:39 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -9,23 +9,48 @@ logger = logging.getLogger(__name__)
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from translation import Translation
-from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
+from translation import Translation as tr
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
+from config import Config
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
+UPDATES_CHANNEL = C.UPDATES_CHANNEL
 
 
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["about"]))
-async def help_user(bot, update):
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.ABOUT_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True   
+@Client.on_message(filters.incoming & filters.command(['about']))
+async def _about(client, message):
+        update_channel = UPDATES_CHANNEL
+    if update_channel:
+        try:
+            user = client.get_chat_member(update_channel, message.chat.id)
+            if user.status == "kicked":
+               client.send_message(
+                   chat_id=message.chat.id,
+                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/UniversalBotsSupport).",
+                   parse_mode="markdown",
+                   disable_web_page_preview=True
+               )
+               return
+        except UserNotParticipant:
+            client.send_message(
+                chat_id=message.chat.id,
+                text="**Please Join My Updates Channel to use this Bot!**",
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{update_channel}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            client.send_message(
+                chat_id=update.chat.id,
+                text=Translation.ABOUT_TEXT,
+               parse_mode="html",
+               reply_to_message_id=update.message_id,
+               disable_web_page_preview=True   
     ) 
