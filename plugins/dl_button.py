a8d3144fa58375477224514c9663eac92b0bfce4
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-08-01 09:03:33 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -89,7 +89,7 @@ async def ddl_call_back(bot, update):
             logger.info(youtube_dl_url)
             logger.info(custom_file_name)
         else:
-        for entity in update.message.reply_to_message.entities:
+            for entity in update.message.reply_to_message.entities:
         if entity.type == "text_link":
                 youtube_dl_url = entity.url
         elif entity.type == "url":
@@ -104,14 +104,14 @@ async def ddl_call_back(bot, update):
                 text=Translation.DOWNLOAD_START,
                 chat_id=update.message.chat.id,
                 message_id=update.message.message_id
-       )
+        )
                 tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
-    if not os.path.isdir(tmp_directory_for_each_user):
-        os.makedirs(tmp_directory_for_each_user)
-    download_directory = tmp_directory_for_each_user + "/" + custom_file_name
-    command_to_exec = []
-    async with aiohttp.ClientSession() as session:
-        c_time = time.time()
+        if not os.path.isdir(tmp_directory_for_each_user):
+                os.makedirs(tmp_directory_for_each_user)
+                download_directory = tmp_directory_for_each_user + "/" + custom_file_name
+                command_to_exec = []
+        async with aiohttp.ClientSession() as session:
+                c_time = time.time()
         try:
             await download_coroutine(
                 bot,
@@ -122,46 +122,46 @@ async def ddl_call_back(bot, update):
                 update.message.message_id,
                 c_time
             )
-        except asyncio.TimeOutError:
+       except asyncio.TimeOutError:
             await bot.edit_message_text(
                 text=Translation.SLOW_URL_DECED,
                 chat_id=update.message.chat.id,
                 message_id=update.message.message_id
             )
             return False
-    if os.path.exists(download_directory):
-        end_one = datetime.now()
-        await bot.edit_message_text(
+      if os.path.exists(download_directory):
+            end_one = datetime.now()
+            await bot.edit_message_text(
             text=Translation.UPLOAD_START,
             chat_id=update.message.chat.id,
             message_id=update.message.message_id
-        )
-        file_size = Config.TG_MAX_FILE_SIZE + 1
-        try:
+     )
+            file_size = Config.TG_MAX_FILE_SIZE + 1
+     try:
             file_size = os.stat(download_directory).st_size
-        except FileNotFoundError as exc:
+     except FileNotFoundError as exc:
             download_directory = os.path.splitext(download_directory)[0] + "." + "mkv"
             # https://stackoverflow.com/a/678242/4723940
             file_size = os.stat(download_directory).st_size
-        if file_size > Config.TG_MAX_FILE_SIZE:
+     if file_size > Config.TG_MAX_FILE_SIZE:
             await bot.edit_message_text(
                 chat_id=update.message.chat.id,
                 text=Translation.RCHD_TG_API_LIMIT,
                 message_id=update.message.message_id
             )
-        else:
+     else:
             # get the correct width, height, and duration for videos greater than 10MB
             # ref: message from @BotSupport
             width = 0
             height = 0
             duration = 0
-            if tg_send_type != "file":
+     if tg_send_type != "file":
                 metadata = extractMetadata(createParser(download_directory))
-                if metadata is not None:
+     if metadata is not None:
                     if metadata.has("duration"):
                         duration = metadata.get('duration').seconds
             # get the correct width, height, and duration for videos greater than 10MB
-            if os.path.exists(thumb_image_path):
+     if os.path.exists(thumb_image_path):
                 width = 0
                 height = 0
                 metadata = extractMetadata(createParser(thumb_image_path))
@@ -185,7 +185,7 @@ async def ddl_call_back(bot, update):
                     img.resize((90, height))
                 img.save(thumb_image_path, "JPEG")
                 # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
-            else:
+     else:
                 thumb_image_path = None
             start_time = time.time()
             # try to upload file
